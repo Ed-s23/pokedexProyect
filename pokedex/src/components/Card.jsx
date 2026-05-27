c9d99df8cfb4da57d19efd84d468d767d44a0613
ED <laloangeles117@gmail.com>
2026-05-26 22:28:58 -0600

    consumo de Api
    ya se cosume la api, se esta por realizar las acciones de filtrado, ya filtra en la cosola, falta mostrarlos en web

--- pokedex/src/components/Card.jsx
+++ pokedex/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import {SyncLoader} from 'react-spinners'
 
 function Card({url,nombre,noPokemon}) {
     const [imagen,setImagen] = useState();
@@ -18,12 +19,14 @@ function Card({url,nombre,noPokemon}) {
     },[]);
 
   return (
-    <li>
-        {imagen && (<img className = " w-[100px] center" src = {imagen}/>)}
-      <div>
+    <li className='transition duration-300 hover:scale-110 h-[180px] mb-8'>
+        {imagen? (<img className = " w-[150px] h-[150px] mb-[-32px]" src = {imagen}/>):
+          <SyncLoader   className='m-auto ' />
+         }
+      <div className='bg-slate-800 text-white p-2 pt-6 shadow-2xl shadow-yellow-100 rounded' >
         
-        <p># {noPokemon}</p>
-        <h2>{nombre}</h2>
+        <p className='text-yellow-300 font-bold text-lg'>#{noPokemon}</p>
+        <h2 className='text-red-400 text-2xl capitalize'>{nombre}</h2>
       </div>
     </li>
   )
--- pokedex/src/pages/ListaPokemons.jsx
+++ pokedex/src/pages/ListaPokemons.jsx
@@ -1,74 +1,92 @@
-// import React, { useEffect, useState } from 'react'
-// //*En esta sona se ejecuta JavaScript
-// const ListaPokemons = () => {
-
-//   // ! Se ejecuta puro jsx, se pueden usar hoocks 
-//   const URL = "https://pokeapi.co/api/v2/pokemon/";
-//   const [pokemon, setPokemons] = useState([]) //! Uso de useState, esperando arreglos 
-//   const obtenerPokemons = async(url) =>{ //*Funcion de flecha para resiclarce
-//     //*fetch reaaliza una peticion, async y awayt 
-//     try {
-//         const resultado = await fetch(url);
-//         const datosPokemon = await resultado.json();
-//         setPokemons(datosPokemon.results);
-//     } catch (error) {
-//       console.error(error);
-      
-//     }
-//   }
-//   useEffect(()=>{
-//       console.log(pokemons);  //*Consultar si se guarda en el hook para pkemons 
-//   },[pokemons]);
-//   //*LLamar funciones, usando useefect 
-//   useEffect(()=> {
-//     obtenerPokemons(URL);
-//   },[])
-
-//   return (
-//     <section>
-//       <ul>
-//         {
-//           pokemons.map((pokemon,indice)=>(
-//           <li>
-//             <h2>{pokemon.name}</h2>
-//           </li> 
-//         ))
-//         }
-//       </ul>
-//     </section>
-//   )
-// }
-
-// export default ListaPokemons
 
 import Card from "../components/Card"
-import { useEffect, useState } from "react"
+import { Children, useContext, useEffect, useState } from "react"
+import {Plus,Search, Target } from 'lucide-react'
 
 const ListaPokemons = () => {
-  const URL = "https://pokeapi.co/api/v2/pokemon/"
-  const [pokemons, setPokemons] = useState([])
 
-  const obtenerPokemons = async (url) => {
+
+  const URL = "https://pokeapi.co/api/v2/pokemon/"
+  const URLFull = "https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0"
+  const [pokemons, setPokemons] = useState([]); 
+  const [next,setNext] = useState(null); //Nuevo hocks
+  const [pokemonName, setPokemonName] = useState("");
+
+ const [todosPokemons,setTodosPokemons] = useState([])
+ 
+  const pokemonByName = todosPokemons.filter(pokemons => 
+    pokemons.name.includes(pokemonName))
+  console.log(pokemonByName)
+   //const {contexto,setContexto} = useContext(value)
+  //! Nuevo estado 
+  const handleChangePokemonName = (e)=> 
+     setPokemonName(e.target.value.toLowerCase())
+  // //  console.log("change")
+
+  
+
+
+
+
+
+ // ! LLamada a todos los pokemons a la hora de la busuqeda 
+
+//   // // const buscadorPrueva = ({children}) = {
+   const allPokemons = async (url)=>{
+       try {
+      const resultado = await fetch(url);
+      const datosPokemon = await resultado.json();
+      setTodosPokemons([...todosPokemons, ...datosPokemon.results]);
+       } catch (error) {
+         console.error(error)
+      }
+
+     }
+   
+
+  //const handleSu
+  const obtenerPokemons = async (url) => {// Obtiene Url 
     try {
-      const resultado = await fetch(url)
-      const datosPokemon = await resultado.json()
-      setPokemons(datosPokemon.results)
+      const resultado = await fetch(url);
+      const datosPokemon = await resultado.json();
+      setPokemons([...pokemons, ...datosPokemon.results]);
+      setNext(datosPokemon.next)
+    
     } catch (error) {
       console.error(error)
     }
   }
 
+
   useEffect(() => {
     console.log(pokemons)
   }, [pokemons])
-
+//! 50  pokemons
   useEffect(() => {
     obtenerPokemons(URL)
   }, [])
+//! Todos los poqumons
+   useEffect(() => {
+     allPokemons(URLFull)
+   }, [])
 
   return (
     <section>
-      <ul className=" flex flex-wrap w-full gap-2">
+      <div className="flex justify-center mb-10" >
+      <form className="flex border-2 border-red-900 px-2">
+        <input 
+        type="text" 
+        name="pokemonName"
+        autoComplete="off"
+        placeholder="Buscar un pokemon" 
+        className="outline-none " 
+        onChange={handleChangePokemonName}
+        /> 
+        <Search />
+      </form>
+      </div>
+
+      <ul className=" flex flex-wrap w-full justify-center gap-2">
         {pokemons.map((pokemon, indice) => (
           <Card 
           key = {indice}
@@ -76,8 +94,19 @@ const ListaPokemons = () => {
           nombre={pokemon.name} 
           noPokemon={indice +1}
           />
+
+
         ))}
+
       </ul>
+      <div className="flex w-full justify-center my-10">
+        {
+          next &&
+          <button onClick={()=> obtenerPokemons(next)}
+          className="flex gap-1 bg-red-900 text-white px-2 py-1 rounded shadow cursor-pointer" 
+        > <Plus />Mostrar mas</button>
+        }
+      </div>
     </section>
   )
 }
